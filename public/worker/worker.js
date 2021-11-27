8a674af421c0edd577391f197eeb1e70f0155fec
Prithwiraj Samanta <prskid1000@gmail.com>
2021-11-27 12:21:46 +0530

    update

--- public/worker/worker.js
+++ public/worker/worker.js
@@ -6,6 +6,7 @@ importScripts("atomProcessor.js")
 importScripts("emptyProcessor.js")
 importScripts("liveProcessor.js")
 self.onmessage = (message) => {
+    console.log(message.data.statement.substring(11))
     var simulate = new Function("message", message.data.statement.substring(11))
     postMessage(simulate(message.data.args))
 }
